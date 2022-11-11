a8cc0040c24ae118ca12240197e20a1a05409af5
alperrkilic <balper78645@hotmail.com>
2022-11-11 19:59:10 +0300

    Update

--- src/LeafArcher.cpp
+++ src/LeafArcher.cpp
@@ -22,7 +22,7 @@ void LeafArcher::init_ani_stats()
 {
 
   this->path += "/Leaf Archer";
-  this->atk_one_num = 10;
+  this->atk_one_num = 15;
   this->atk_two_num = 15;
   this->atk_sp_num = 17;
   this->death_num = 19;
